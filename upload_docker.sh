6977859984367b781460b44c3632dece3410bb06
EC2 Default User <ec2-user@ip-172-31-36-123.ec2.internal>
2023-02-11 20:21:50 +0000

    upload docker image

--- upload_docker.sh
+++ upload_docker.sh
@@ -6,10 +6,15 @@
 # Step 1:
 # Create dockerpath
 # dockerpath=<your docker ID/path>
+dockerpath=imaabasiee/sklearn
 
 # Step 2:  
 # Authenticate & tag
 echo "Docker ID and Image: $dockerpath"
+docker login
+docker tag sklearn $dockerpath:latest
 
 # Step 3:
 # Push image to a docker repository
+docker push $dockerpath:latest
+
